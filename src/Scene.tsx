78d2dae9fc27c656b773960922c3cce7bf4bbc9d
OwnKng <owen.king@outlook.com>
2021-12-11 13:56:04 +0000

    Sleep tweak

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -8,7 +8,7 @@ const Scene = () => (
   <Canvas camera={{ position: [0, 10, 0] }}>
     <hemisphereLight intensity={0.2} />
     <directionalLight intensity={0.4} position={[0, 12, 0]} />
-    <Physics broadphase='SAP' allowSleep>
+    <Physics broadphase='SAP'>
       <Player />
       <Blocks />
     </Physics>
